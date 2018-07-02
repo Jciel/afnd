bc040116a8881d2fea5f5960f365b2dc4913a715
jciel <jcielsouza@gmail.com>
2018-07-01 23:25:59 -0300

    Initiating creation of the string identification function

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject afnd "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Implementation of a finite automaton presented in the discipline Formal Languages \u200B\u200Band Automata of the course of Computer Science"
+  :url "jciel.github.io"
+  :license {:name "MIT"
+            :url ""}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main afnd.core/main)
--- src/afnd/core.clj
+++ src/afnd/core.clj
@@ -26,12 +26,17 @@
 (defn read-initial-state
   []
   (println "Digite o estado inicial")
-  (read-data-afnd))
+  (keyword (read-data-afnd)))
 
 (defn read-final-states
   []
   (println "Digite os estados finais")
   (read-data-afnd))
+
+(defn read-string-chain
+  []
+  (let [chain (read-line)]
+    (s/split chain #"")))
 
 (defn mount-rule
   [rule]
@@ -50,15 +55,27 @@
             (recur (conj rules real-rule))))
         rules))))
 
+;(defn define-function-transition
+;  [fn-transition]
+;  (let [fn-part (s/split fn-transition #"")]
+;    (conj {} {(keyword (first fn-part)) (last fn-part)})))
+;
+;(defn mount-part-afnd
+;  [rule]
+;  (let [state (first rule)
+;        fn-transictions (into {} (map define-function-transition (rest rule)))]
+;    {(keyword state) fn-transictions}))
+
+
 (defn define-function-transition
   [fn-transition]
   (let [fn-part (s/split fn-transition #"")]
-    (conj {} {(keyword (first fn-part)) (last fn-part)})))
+    {(keyword (first fn-part)) (last fn-part)}))
 
 (defn mount-part-afnd
   [rule]
   (let [state (first rule)
-        fn-transictions (into {} (map define-function-transition (rest rule)))]
+        fn-transictions (mapv define-function-transition (rest rule))]
     {(keyword state) fn-transictions}))
 
 (defn create-afnd
@@ -68,14 +85,32 @@
       (recur (conj afnd (mount-part-afnd rule-grammar)) (rest rules-grammar))
       afnd)))
 
-
 (defn read-automaton-data
   []
   {:alphabet (read-alphabet)
    :states (read-states)
    :initial_states (read-initial-state)
    :final-states (read-final-states)
-   :rules-grammar (read-rules-grammar [])})
+   :rules-grammar (read-rules-grammar [])
+   :string (read-string-chain)})
+
+
+(defn identifier-chain
+  [chain afnd state]
+  (let [label (first chain)
+        fn-transaction (state afnd)
+        state (filterv #(not (nil? %)) (map #(keyword ((keyword label) %)) fn-transaction))]
+    (println chain)
+    (println label)
+    (println state)
+    (if (empty? state)
+      (println "Label nao encontrada")
+      (map (fn [x]
+             (identifier-chain (rest chain) afnd x)) state))
+    ))
+
+;   {:S [{:a A} {:a B} {:b B}], :A [{:a A} {:b B}], :B [{:a B}]}
+
 
 (defn main
   []
